3e0d6bac473141e5d880cc2ae90f9c4250def601
n-orris <e.vannorris99@gmail.com>
2020-05-30 21:26:14 -0700

    new edit and delete article features added

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+    include ArticlesHelper
+
     def index
         @articles = Article.all
      end
@@ -7,5 +9,42 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
      end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(
+            title: params[:article][:title],
+            body: params[:article][:body])
+          @article.save
+          flash.notice = "New article created!"
+          redirect_to article_path(@article)
+        
+      
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        flash.notice = "Article Deleted!"
+        redirect_to "/articles"
+
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        @article.update(article_params)
+
+        flash.notice = "Article '#{@article.title}' Updated!"
+      
+        redirect_to article_path(@article)
+    end
+    
  end
 
